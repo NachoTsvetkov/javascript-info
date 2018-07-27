052be3d23ef25b94cab90c33cd4b5a0dcf1e31b5
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-07-27 17:43:51 +0300

    inputs completed

---
+++ 1_Fundamentials/input.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// let age = prompt("How old are you?");
+// alert(age);
+
+// let safeAge = prompt("How old are you?", "");
+// alert(safeAge);
+
+// let isBoss = confirm("Are you the boss?");
+// alert(`Is boss ${isBoss}`);
+
+let name = prompt("How do friends call you?", ``);
+alert(`Hi, ${name}`);
