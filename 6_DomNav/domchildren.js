174938d90e01492f1782ca96362f0ae342223f75
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-09 18:31:20 +0300

    new files

---
+++ 6_DomNav/domchildren.js
@@ -0,0 +1,8 @@
+// The <div> DOM node?
+console.log(document.body.children[0]);
+
+// The <ul> DOM node?
+console.log(document.body.children[1]);
+
+// The second <li> (with Pete)?
+console.log(document.body.children[1].children[1]);
